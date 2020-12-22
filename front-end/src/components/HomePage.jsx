e2ba04e3a37e40ac04ad15d22ab1fa4ff1de06b5
paigelindahl <paigemarlindahl@gmail.com>
2020-12-22 14:14:21 +0000

    new background pic

--- front-end/src/components/HomePage.jsx
+++ front-end/src/components/HomePage.jsx
@@ -6,10 +6,12 @@ export function HomePage(props) {
 
   return (
     <>
+    <div className="homepage-background">
       <div className="name-title">
         <h1>Paige Lindahl</h1>
         <h4>Web Developer</h4>
       </div>
+    </div>
     </>
   )
 }
