2197e83b7baa9fea27fee39d04e2886e0d9b769c
paigelindahl <paigemarlindahl@gmail.com>
2021-02-24 15:08:29 +0000

    adds github buttons to projects

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { 
+import {
   BrowserRouter as Router,
   Route
 } from "react-router-dom";
 import { HomePage } from './components/HomePage';
 import Blocks from './components/Blocks';
+import { ChakraProvider } from "@chakra-ui/react"
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/blocks" component={Blocks} />
-      </div>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={HomePage} />
+          <Route path="/blocks" component={Blocks} />
+        </div>
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- front-end/src/components/FadeExample.jsx
+++ front-end/src/components/FadeExample.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import { Button } from "@chakra-ui/react"
+import { FaGithub } from 'react-icons/fa';
+
 import '../styles/FadeExample.scss';
 
 class FadeExample extends React.Component {
@@ -8,15 +11,21 @@ class FadeExample extends React.Component {
       <div>
         <Fade left>
           <div className="project-container">
-            <div class="img-container">
+            <div className="img-container">
               <div className="circle" style={{ backgroundImage: `url(${this.props.image})` }}>
                 {/* <img src={this.props.image} width={this.props.wid} height={this.props.high} /> */}
               </div>
             </div>
             <div className="text-container">
               <h2 className="project-title">{this.props.title}</h2>
-              <p className="project-text">{this.props.text}</p>
+              <hr />
               <p className="project-text">{this.props.language}</p>
+              <p className="project-text">{this.props.text}</p>
+              <div className="github-btn-container">
+                <Button leftIcon={<FaGithub />} colorScheme="teal" variant="solid">
+                  GitHub
+                </Button>
+              </div>
             </div>
           </div>
         </Fade>
